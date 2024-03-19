1166efbe22abe6518a3e1b4d86f4b2a38ec736cf
Dimitrilhostis <103448862+Dimitrilhostis@users.noreply.github.com>
2024-03-19 15:04:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,330 @@
+function f1() {
+    const reponse = "a";
+    let suggest = document.getElementById("r1");
+    let p = document.getElementById("p1");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f2() {
+    const reponse = "a";
+    let suggest = document.getElementById("r2");
+    let p = document.getElementById("p2");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f3() {
+    const reponse = "a";
+    let suggest = document.getElementById("r3");
+    let p = document.getElementById("p3");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f4() {
+    const reponse = "a";
+    let suggest = document.getElementById("r4");
+    let p = document.getElementById("p4");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f5() {
+    const reponse = "a";
+    let suggest = document.getElementById("r5");
+    let p = document.getElementById("p5");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f6() {
+    const reponse = "a";
+    let suggest = document.getElementById("r6");
+    let p = document.getElementById("p6");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function fè() {
+    const reponse = "a";
+    let suggest = document.getElementById("r7");
+    let p = document.getElementById("p7");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f8() {
+    const reponse = "a";
+    let suggest = document.getElementById("r8");
+    let p = document.getElementById("p8");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f9() {
+    const reponse = "a";
+    let suggest = document.getElementById("r9");
+    let p = document.getElementById("p9");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f0() {
+    const reponse = "a";
+    let suggest = document.getElementById("r10");
+    let p = document.getElementById("p10");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f11() {
+    const reponse = "a";
+    let suggest = document.getElementById("r11");
+    let p = document.getElementById("p11");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f12() {
+    const reponse = "a";
+    let suggest = document.getElementById("r12");
+    let p = document.getElementById("p12");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f13() {
+    const reponse = "a";
+    let suggest = document.getElementById("r13");
+    let p = document.getElementById("p13");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f14() {
+    const reponse = "a";
+    let suggest = document.getElementById("r14");
+    let p = document.getElementById("p14");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f15() {
+    const reponse = "a";
+    let suggest = document.getElementById("r15");
+    let p = document.getElementById("p15");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f16() {
+    const reponse = "a";
+    let suggest = document.getElementById("r16");
+    let p = document.getElementById("p16");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f17() {
+    const reponse = "a";
+    let suggest = document.getElementById("r17");
+    let p = document.getElementById("p17");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f18() {
+    const reponse = "a";
+    let suggest = document.getElementById("r18");
+    let p = document.getElementById("p18");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f19() {
+    const reponse = "a";
+    let suggest = document.getElementById("r19");
+    let p = document.getElementById("p19");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f20() {
+    const reponse = "a";
+    let suggest = document.getElementById("r20");
+    let p = document.getElementById("p20");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f21() {
+    const reponse = "a";
+    let suggest = document.getElementById("r21");
+    let p = document.getElementById("p21");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f22() {
+    const reponse = "a";
+    let suggest = document.getElementById("r22");
+    let p = document.getElementById("p22");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f23() {
+    const reponse = "a";
+    let suggest = document.getElementById("r23");
+    let p = document.getElementById("p23");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f24() {
+    const reponse = "a";
+    let suggest = document.getElementById("r24");
+    let p = document.getElementById("p24");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f25() {
+    const reponse = "a";
+    let suggest = document.getElementById("r25");
+    let p = document.getElementById("p25");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f26() {
+    const reponse = "a";
+    let suggest = document.getElementById("r26");
+    let p = document.getElementById("p26");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f27() {
+    const reponse = "a";
+    let suggest = document.getElementById("r27");
+    let p = document.getElementById("p27");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f28() {
+    const reponse = "a";
+    let suggest = document.getElementById("r28");
+    let p = document.getElementById("p28");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f29() {
+    const reponse = "a";
+    let suggest = document.getElementById("r29");
+    let p = document.getElementById("p29");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
+function f30() {
+    const reponse = "a";
+    let suggest = document.getElementById("r30");
+    let p = document.getElementById("p30");
+    if (suggest.value == reponse) {
+        p.textContent = "✅";
+    }
+    else {
+        p.textContent = "❌";
+    }
+}
