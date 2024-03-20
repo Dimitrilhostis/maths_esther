8110a570bcf4d4a214869b989f17d6338e6c889e
Dimitrilhostis <103448862+Dimitrilhostis@users.noreply.github.com>
2024-03-20 01:54:17 +0100

    replace

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function f1() {
-    const reponse = "a";
+function f1(value) {
+    const reponse = value;
     let suggest = document.getElementById("r1");
     let p = document.getElementById("p1");
+    console.log(reponse)
     if (suggest.value == reponse) {
         p.textContent = "✅";
     }
@@ -9,8 +10,8 @@ function f1() {
         p.textContent = "❌";
     }
 }
-function f2() {
-    const reponse = "a";
+function f2(value) {
+    const reponse = value;
     let suggest = document.getElementById("r2");
     let p = document.getElementById("p2");
     if (suggest.value == reponse) {
@@ -20,8 +21,8 @@ function f2() {
         p.textContent = "❌";
     }
 }
-function f3() {
-    const reponse = "a";
+function f3(value) {
+    const reponse = value;
     let suggest = document.getElementById("r3");
     let p = document.getElementById("p3");
     if (suggest.value == reponse) {
@@ -31,8 +32,8 @@ function f3() {
         p.textContent = "❌";
     }
 }
-function f4() {
-    const reponse = "a";
+function f4(value) {
+    const reponse = value;
     let suggest = document.getElementById("r4");
     let p = document.getElementById("p4");
     if (suggest.value == reponse) {
@@ -42,8 +43,8 @@ function f4() {
         p.textContent = "❌";
     }
 }
-function f5() {
-    const reponse = "a";
+function f5(value) {
+    const reponse = value;
     let suggest = document.getElementById("r5");
     let p = document.getElementById("p5");
     if (suggest.value == reponse) {
@@ -53,8 +54,8 @@ function f5() {
         p.textContent = "❌";
     }
 }
-function f6() {
-    const reponse = "a";
+function f6(value) {
+    const reponse = value;
     let suggest = document.getElementById("r6");
     let p = document.getElementById("p6");
     if (suggest.value == reponse) {
@@ -64,8 +65,8 @@ function f6() {
         p.textContent = "❌";
     }
 }
-function fè() {
-    const reponse = "a";
+function f7(value) {
+    const reponse = value;
     let suggest = document.getElementById("r7");
     let p = document.getElementById("p7");
     if (suggest.value == reponse) {
@@ -75,8 +76,8 @@ function fè() {
         p.textContent = "❌";
     }
 }
-function f8() {
-    const reponse = "a";
+function f8(value) {
+    const reponse = value;
     let suggest = document.getElementById("r8");
     let p = document.getElementById("p8");
     if (suggest.value == reponse) {
@@ -86,8 +87,8 @@ function f8() {
         p.textContent = "❌";
     }
 }
-function f9() {
-    const reponse = "a";
+function f9(value) {
+    const reponse = value;
     let suggest = document.getElementById("r9");
     let p = document.getElementById("p9");
     if (suggest.value == reponse) {
@@ -97,8 +98,8 @@ function f9() {
         p.textContent = "❌";
     }
 }
-function f0() {
-    const reponse = "a";
+function f10(value) {
+    const reponse = value;
     let suggest = document.getElementById("r10");
     let p = document.getElementById("p10");
     if (suggest.value == reponse) {
@@ -108,8 +109,8 @@ function f0() {
         p.textContent = "❌";
     }
 }
-function f11() {
-    const reponse = "a";
+function f11(value) {
+    const reponse = value;
     let suggest = document.getElementById("r11");
     let p = document.getElementById("p11");
     if (suggest.value == reponse) {
@@ -119,8 +120,8 @@ function f11() {
         p.textContent = "❌";
     }
 }
-function f12() {
-    const reponse = "a";
+function f12(value) {
+    const reponse = value;
     let suggest = document.getElementById("r12");
     let p = document.getElementById("p12");
     if (suggest.value == reponse) {
@@ -130,8 +131,8 @@ function f12() {
         p.textContent = "❌";
     }
 }
-function f13() {
-    const reponse = "a";
+function f13(value) {
+    const reponse = value;
     let suggest = document.getElementById("r13");
     let p = document.getElementById("p13");
     if (suggest.value == reponse) {
@@ -141,8 +142,8 @@ function f13() {
         p.textContent = "❌";
     }
 }
-function f14() {
-    const reponse = "a";
+function f14(value) {
+    const reponse = value;
     let suggest = document.getElementById("r14");
     let p = document.getElementById("p14");
     if (suggest.value == reponse) {
@@ -152,8 +153,8 @@ function f14() {
         p.textContent = "❌";
     }
 }
-function f15() {
-    const reponse = "a";
+function f15(value) {
+    const reponse = value;
     let suggest = document.getElementById("r15");
     let p = document.getElementById("p15");
     if (suggest.value == reponse) {
@@ -163,8 +164,8 @@ function f15() {
         p.textContent = "❌";
     }
 }
-function f16() {
-    const reponse = "a";
+function f16(value) {
+    const reponse = value;
     let suggest = document.getElementById("r16");
     let p = document.getElementById("p16");
     if (suggest.value == reponse) {
@@ -174,8 +175,8 @@ function f16() {
         p.textContent = "❌";
     }
 }
-function f17() {
-    const reponse = "a";
+function f17(value) {
+    const reponse = value;
     let suggest = document.getElementById("r17");
     let p = document.getElementById("p17");
     if (suggest.value == reponse) {
@@ -185,8 +186,8 @@ function f17() {
         p.textContent = "❌";
     }
 }
-function f18() {
-    const reponse = "a";
+function f18(value) {
+    const reponse = value;
     let suggest = document.getElementById("r18");
     let p = document.getElementById("p18");
     if (suggest.value == reponse) {
@@ -196,8 +197,8 @@ function f18() {
         p.textContent = "❌";
     }
 }
-function f19() {
-    const reponse = "a";
+function f19(value) {
+    const reponse = value;
     let suggest = document.getElementById("r19");
     let p = document.getElementById("p19");
     if (suggest.value == reponse) {
@@ -207,8 +208,8 @@ function f19() {
         p.textContent = "❌";
     }
 }
-function f20() {
-    const reponse = "a";
+function f20(value) {
+    const reponse = value;
     let suggest = document.getElementById("r20");
     let p = document.getElementById("p20");
     if (suggest.value == reponse) {
@@ -218,8 +219,8 @@ function f20() {
         p.textContent = "❌";
     }
 }
-function f21() {
-    const reponse = "a";
+function f21(value) {
+    const reponse = value;
     let suggest = document.getElementById("r21");
     let p = document.getElementById("p21");
     if (suggest.value == reponse) {
@@ -229,8 +230,8 @@ function f21() {
         p.textContent = "❌";
     }
 }
-function f22() {
-    const reponse = "a";
+function f22(value) {
+    const reponse = value;
     let suggest = document.getElementById("r22");
     let p = document.getElementById("p22");
     if (suggest.value == reponse) {
@@ -240,8 +241,8 @@ function f22() {
         p.textContent = "❌";
     }
 }
-function f23() {
-    const reponse = "a";
+function f23(value) {
+    const reponse = value;
     let suggest = document.getElementById("r23");
     let p = document.getElementById("p23");
     if (suggest.value == reponse) {
@@ -251,8 +252,8 @@ function f23() {
         p.textContent = "❌";
     }
 }
-function f24() {
-    const reponse = "a";
+function f24(value) {
+    const reponse = value;
     let suggest = document.getElementById("r24");
     let p = document.getElementById("p24");
     if (suggest.value == reponse) {
@@ -262,8 +263,8 @@ function f24() {
         p.textContent = "❌";
     }
 }
-function f25() {
-    const reponse = "a";
+function f25(value) {
+    const reponse = value;
     let suggest = document.getElementById("r25");
     let p = document.getElementById("p25");
     if (suggest.value == reponse) {
@@ -273,8 +274,8 @@ function f25() {
         p.textContent = "❌";
     }
 }
-function f26() {
-    const reponse = "a";
+function f26(value) {
+    const reponse = value;
     let suggest = document.getElementById("r26");
     let p = document.getElementById("p26");
     if (suggest.value == reponse) {
@@ -284,8 +285,8 @@ function f26() {
         p.textContent = "❌";
     }
 }
-function f27() {
-    const reponse = "a";
+function f27(value) {
+    const reponse = value;
     let suggest = document.getElementById("r27");
     let p = document.getElementById("p27");
     if (suggest.value == reponse) {
@@ -295,8 +296,8 @@ function f27() {
         p.textContent = "❌";
     }
 }
-function f28() {
-    const reponse = "a";
+function f28(value) {
+    const reponse = value;
     let suggest = document.getElementById("r28");
     let p = document.getElementById("p28");
     if (suggest.value == reponse) {
@@ -306,8 +307,8 @@ function f28() {
         p.textContent = "❌";
     }
 }
-function f29() {
-    const reponse = "a";
+function f29(value) {
+    const reponse = value;
     let suggest = document.getElementById("r29");
     let p = document.getElementById("p29");
     if (suggest.value == reponse) {
@@ -317,8 +318,8 @@ function f29() {
         p.textContent = "❌";
     }
 }
-function f30() {
-    const reponse = "a";
+function f30(value) {
+    const reponse = value;
     let suggest = document.getElementById("r30");
     let p = document.getElementById("p30");
     if (suggest.value == reponse) {
@@ -326,5 +327,185 @@ function f30() {
     }
     else {
         p.textContent = "❌";
+    }
+}
+function rep1(){
+    let rep = document.getElementById("rep1")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep10(){
+    let rep = document.getElementById("rep10")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep2(){
+    let rep = document.getElementById("rep2")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep3(){
+    let rep = document.getElementById("rep3")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep4(){
+    let rep = document.getElementById("rep4")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep5(){
+    let rep = document.getElementById("rep5")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep6(){
+    let rep = document.getElementById("rep6")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep7(){
+    let rep = document.getElementById("rep7")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep8(){
+    let rep = document.getElementById("rep8")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep9(){
+    let rep = document.getElementById("rep9")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep11(){
+    let rep = document.getElementById("rep11")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep12(){
+    let rep = document.getElementById("rep12")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep13(){
+    let rep = document.getElementById("rep13")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep14(){
+    let rep = document.getElementById("rep14")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep15(){
+    let rep = document.getElementById("rep15")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep16(){
+    let rep = document.getElementById("rep16")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep17(){
+    let rep = document.getElementById("rep17")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep18(){
+    let rep = document.getElementById("rep18")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep19(){
+    let rep = document.getElementById("rep19")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
+    }
+}
+function rep20(){
+    let rep = document.getElementById("rep20")
+    if (rep.style.opacity == 0){
+        rep.style.opacity = 1
+    }
+    else{
+        rep.style.opacity = 0
     }
 }
